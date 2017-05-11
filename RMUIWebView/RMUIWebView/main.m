bc14750fd08dc9e53c55f3e61437cd2f425237a5
shea <401603604@qq.com>
2017-05-11 16:55:29 +0800

    Learning exercise, communication

---
+++ RMUIWebView/RMUIWebView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RMUIWebView
+//
+//  Created by rm on 2017/5/11.
+//  Copyright © 2017年 rm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ RMUIWebView/RMUIWebViewTests/RMUIWebViewTests.m
@@ -0,0 +1,39 @@
+//
+//  RMUIWebViewTests.m
+//  RMUIWebViewTests
+//
+//  Created by rm on 2017/5/11.
+//  Copyright © 2017年 rm. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RMUIWebViewTests : XCTestCase
+
+@end
+
+@implementation RMUIWebViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
